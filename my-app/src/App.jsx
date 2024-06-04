de6a025d6b65fd92b868bdbabb00533f14992ab7
shibiyas <shibiyas7@gmail.com>
2024-06-04 17:30:08 +0530

    add

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
  import './App.css'
 import './Register.css'
 import Register from './Register'
-import PopupRegistrationBox from './PopupRegistrationBox'
+// import PopupRegistrationBox from './PopupRegistrationBox'
+// import Selectbox from './Select'
+// import SearchComponent from './Select'
+
 
 function App() {
 
 
   return (
     <>
-    {/* <Register/> */}
-    <PopupRegistrationBox/>
+    <Register/>
+    {/* <PopupRegistrationBox/> */}
+    {/* <SearchComponent/> */}
     </>
   )
 }
--- my-app/src/Register.jsx
+++ my-app/src/Register.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Register.css'
 import { Modale } from "npm-modale-lib-react"; 
+import TextField from '@mui/material/TextField';
+import { FormControl, Select, MenuItem, InputLabel } from '@mui/material';
+import axios from 'axios';
+
+
+
 
 
 
 const Register = () => {
 
+
     // depent dropdow
-    const[selectprefix,setSelectedPrefix]=useState('')
-    const[selectgender,setSelectedGender]=useState('')
+    const [selectPrefix, setSelectedPrefix] = useState('');
+  const [selectGender, setSelectedGender] = useState('');
 
     const genderdata={
         MR:'Male',
@@ -47,6 +54,40 @@ const Register = () => {
         console.log("Registration logic goes here");
         closePopup(); // Close the popup after registration
     }
+
+    // const classes = useStyles();
+    const [date, setDate] = useState('');
+    const [day, setDay] = useState('');
+    const [month, setMonth] = useState('');
+    const [year, setYear] = useState('');
+  
+    const handleDateChange = (event) => {
+      const selectedDate = event.target.value;
+      setDate(selectedDate);
+  
+      // Split the date into components
+      const [yyyy, mm, dd] = selectedDate.split('-');
+      setDay(dd);
+      setMonth(mm);
+      setYear(yyyy);
+    };
+
+    // show data
+    const [Getdata,setData]=useState([])
+    const getAlldata=async()=>{
+      const res=await axios.get(" http://172.16.16.10:8082/api/PatientMstr/PatientSearchMaster")
+      setData(res.data)
+      console.log(Getdata);
+    }
+    useEffect(()=>{
+      getAlldata()
+    },[])
+
+
+
+  
+    
+  
     
     
   return (
@@ -60,71 +101,112 @@ const Register = () => {
       <div className="all">
         <h1>Registration Form</h1>
       </div>
-      {/* <div className="search">
+      <div className="search">
+      <FormControl>
+        <InputLabel id="prefix-label">Prefix</InputLabel>
+        <Select
+          labelId="prefix-label"
+          id="prefix-select"
+          value={selectPrefix}
+          style={{ height: '40px',width: '120px' }}
+          onChange={handlePrefixChange}
+        >
+         <MenuItem value="patname">Name</MenuItem>
+          <MenuItem value="patid">Patient ID</MenuItem>
+          <MenuItem value="phone">Phone No</MenuItem>
+          <MenuItem value="email">Email</MenuItem>
+        </Select>
+      </FormControl>
+      
+      <TextField id="outlined-basic searchinp" label="Search patient" variant="outlined" InputProps={{ style: { height: '40px' ,width:'300px',marginLeft:'10px' } }}
+             InputLabelProps={{ style: { fontSize: '14px', marginTop: '-4px' } }} className='searchs' />
         <button>Search</button>
-        <input type="text" placeholder='Search Patient ' />
-      </div> */}
-      <div className="form-floating mb-1 p-1 search">
-     
-                <input 
-                    type="email" 
-                    className="form-control" 
-                    id="floatingInput" 
-                    placeholder="name@example.com" 
-                />
-                <label htmlFor="floatingInput">Search Patient</label>
-                 <button>Search</button>
-            </div>
+      </div>
       <div className="id">
-        <input type="text" name="" id="" placeholder='UHID' />
+
+        <TextField id="outlined-basic" label="UHID" variant="outlined" InputProps={{ style: { height: '40px' ,width:'100px' } }}
+             InputLabelProps={{ style: { fontSize: '14px', marginTop: '-4px' } }}  className='uhid' />
       </div>
       <div className="heading">
         <h3>Patient Details:</h3>
       </div>
-      <form action="" onSubmit={handleRegister} onChange={handlePrefixChange}>
+     <div className="maping">
+      {
+        Getdata.map((data,index)=>{
+          <div className="show" key={index}>
+             <form action="" onSubmit={handleRegister} onChange={handlePrefixChange}>
       <div className="name">
-        <select name="" id="" value={selectprefix}>
-            <option value="MR">MR</option>
-            <option value="MRS">MRS</option>
-        </select>
-        <div className="form-floating mb-1 p-1 search">
-     
-     <input 
-         type="email" 
-         className="form-control" 
-         id="floatingInput" 
-         placeholder="name@example.com" 
-     />
-     <label htmlFor="floatingInput">Name</label>
-      {/* <button>Search</button> */}
- </div>
+      <FormControl>
+        <InputLabel id="prefix-label">Prefix</InputLabel>
+        <Select
+          labelId="prefix-label"
+          id="prefix-select"
+          value={selectPrefix}
+          style={{ height: '40px',width: '85px' }}
+          onChange={handlePrefixChange}
+        >
+          <MenuItem value="p">prefix</MenuItem>
+          <MenuItem value="MR">MR</MenuItem>
+          <MenuItem value="MRS">MRS</MenuItem>
+        </Select>
+      </FormControl>
+   
+        <TextField id="outlined-basic" label="Patient Name" variant="outlined" InputProps={{ style: { height: '40px' ,width:'100%' } }}
+             InputLabelProps={{ style: { fontSize: '14px', marginTop: '-4px' } }}  className='names' />
       </div>
 
       <div className="detail">
         <div className="gender">
-            <select name="" id="" value={selectgender} onChange={handleGenderChange}>
-                <option value=""selected>Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-            </select>
-            <input type="date" className='dob' placeholder='DOB'/>
-            <input type="text" className='dd' placeholder='DD' />
-            <input type="text" className='mm' placeholder='MM' />
-            <input type="text" className='yy' placeholder='YY' />
+        <FormControl>
+        <InputLabel id="gender-label">Gender</InputLabel>
+        <Select
+          labelId="gender-label"
+          id="gender-select"
+          value={selectGender}
+          style={{ height: '40px',width: '85px'  }}
+          onChange={handleGenderChange}
+        >
+          <MenuItem value="">Gender</MenuItem>
+          <MenuItem value="Male">Male</MenuItem>
+          <MenuItem value="Female">Female</MenuItem>
+        </Select>
+      </FormControl>
+            <TextField type='date'  value={date} onChange={handleDateChange} InputLabelProps={{
+          shrink: true, }} id="outlined-basic" label="Date of Birth" variant="outlined"  InputProps={{ style: { height: '40px' ,width:'100%' } }}className='dates' />
+            <TextField id="outlined-basic" label="DD" variant="outlined" className='dd'   value={day}
+        InputLabelProps={{
+          shrink: true,
+        }} InputProps={{ style: { height: '40px' ,width:'100%' } }} />
+            <TextField id="outlined-basic" label="MM" variant="outlined" className='dd'   value={month}
+        InputLabelProps={{
+          shrink: true,
+        }} InputProps={{ style: { height: '40px' ,width:'100%' } }} />
+            <TextField id="outlined-basic" label="YY" variant="outlined" className='dd'   value={year}
+        InputLabelProps={{
+          shrink: true,
+        }} InputProps={{ style: { height: '40px' ,width:'100%' } }}/>
         </div>
       </div>
       <div className="sec-detail">
-        <input type="text" className='phone' placeholder='Phone Number'/>
-        <input type="text" className='email' placeholder='Email' />
+      <TextField id="outlined-basic" label="Phone Number" InputProps={{ style: { height: '40px' ,width:'250px' } }}
+             InputLabelProps={{ style: { fontSize: '14px', marginTop: '-5px' } }} variant="outlined" />
+      <div className="email">
+      <TextField id="outlined-basic" label="Email" InputProps={{ style: { height: '40px' ,width:'250px' } }}
+             InputLabelProps={{ style: { fontSize: '14px', marginTop: '-5px' } }} variant="outlined" />
       </div>
-      <div className="form-floating address">
-  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-  <label htmlFor="floatingTextarea">Address</label>
-</div>
-<div className="form-floating address">
-  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-  <label htmlFor="floatingTextarea">Notes</label>
-</div>
+      </div>
+      <div className="address">
+          <TextField id="outlined-multiline-flexible" label="Address"  multiline  maxRows={4}
+         className='adresses'/>
+      </div>
+      <div className="address">
+      <TextField id="outlined-multiline-flexible" label="Notes"  multiline  maxRows={4}
+         className='adresses'/>
+      </div>
+      <div className="add-detail">
+        <a href="">Additional Detail</a>
+      </div>
+      
       <div className="buttons">
         <button id='new'>New</button>
         <button id='bill'>Proceed to bill</button>
@@ -134,6 +216,10 @@ const Register = () => {
 
       </div>
       </form>
+          </div>
+        })
+      }
+     </div>
       </div>
 }
     </div>
